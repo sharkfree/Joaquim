3b82599c4b1b56a192d8ac5c84a9d89c213aadc5
shark <vwildt@hotmail.de>
2025-09-09 13:44:21 +0200

    bonito

--- src/src/Bundchen.java
+++ src/src/Bundchen.java
@@ -5,7 +5,7 @@ public class Bundchen {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("bundchen joaquim merda tarada iate moreno alto");
+		System.out.println("bundchen joaquim merda tarada iate moreno alto bonito");
 	}
 
 }
