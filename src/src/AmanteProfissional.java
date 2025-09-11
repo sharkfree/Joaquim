561ee8c7e19326c009e5cf69fe4a7ee464884511
shark <vwildt@hotmail.de>
2025-09-11 08:30:33 +0200

    do

--- src/src/AmanteProfissional.java
+++ src/src/AmanteProfissional.java
@@ -1,13 +1,13 @@
 package src;
 
-public class Bundchen {
+public class AmanteProfissional {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 		System.out.println(
 	"bundchen joaquim merda tarada iate"
-	+ " moreno alto bonito profissional amante o endereco para vadia trouxa");
+	+ " moreno alto bonito profissional amante o endereco para vadia trouxa do");
 	}
 
 }
