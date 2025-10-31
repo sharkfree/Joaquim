b006b06a741063f31388b25383053beea6a54306
shark <vwildt@hotmail.de>
2025-10-31 11:29:39 +0100

    programa

--- src/src/AmanteProfissional.java
+++ src/src/AmanteProfissional.java
@@ -6,7 +6,7 @@ public class AmanteProfissional {
 		// TODO Auto-generated method stub
 
 		System.out.println(
-	"bundchen joaquim merda tarada iate gostoso garoto de "
+	"bundchen joaquim merda tarada iate gostoso garoto de programa"
 
 	);
 
